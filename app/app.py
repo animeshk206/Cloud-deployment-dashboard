03564433f1e3383f025474d9fad6c3ac16048e13
Animesh Kumar Singh <19311a04v0@sreenidhi.edu.in>
2026-02-22 12:40:40 +0000

    app.oy

--- app/app.py
+++ app/app.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "Cloudops tracker Running"}
+
+deployments = []
+ 
+class Deployment(BaseModel):
+    service_name: str
+    environment: str
+    version: str
+    status: str
+ 
+@app.post("/deploy")
+def create_deployment(deployment: Deployment):
+    deployments.append(deployment)
+    return {"message": "Deployment recorded", "data": deployment}
+ 
+@app.get("/deployments")
+def get_deployments():
+    return deployments
+ 
